5026f8b7756e03540179720eb69ab666519a7974
DaEunKim <ekdms717@naver.com>
2025-05-30 09:53:05 +0900

    first commit

---
+++ src/main/java/com/dani/raginmind/RaGinMindApplication.java
@@ -0,0 +1,13 @@
+package com.dani.raginmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaGinMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaGinMindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dani/raginmind/RaGinMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dani.raginmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaGinMindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
